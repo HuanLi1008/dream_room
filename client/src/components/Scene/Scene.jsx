92122662ce4639425e77509c5af5ffb44838e6fa
Heppner <Heppner@Rebekahs-MacBook-Pro.local>
2024-03-07 08:19:42 -0800

    Add component files and jsx structure

---
+++ client/src/components/Scene/Scene.jsx
@@ -0,0 +1,12 @@
+function Scene() {
+  return (
+    <section className="scene">
+      <img className="scene__bed" src="" alt=""></img>
+      <img className="scene__wall" src="" alt=""></img>
+      <img className="scene__floor" src="" alt=""></img>
+      <img className="scene__rug" src="" alt=""></img>
+      <img className="scene__window" src="" alt=""></img>
+      <img className="scene__more" src="" alt=""></img>
+    </section>
+  );
+}
