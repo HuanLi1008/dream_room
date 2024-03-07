512665ee714108f3aaee9a922dbe9ea09995e91c
Heppner <Heppner@Rebekahs-MacBook-Pro.local>
2024-03-07 08:45:01 -0800

    Add optionsmenu component

---
+++ client/src/components/OptionsMenu/OptionsMenu.jsx
@@ -0,0 +1,12 @@
+function OptionsMenu() {
+
+    return (
+        <section className="ui-menu__options">
+          <div className="ui-menu__options-start"></div>
+          <div className="ui-menu__options-end"></div>
+          <div className="ui-menu__options-reset"></div>
+          <div className="ui-menu__options-download"></div>
+        </section>
+    )
+}
+export default OptionsMenu;
--- client/src/components/Ui/Ui.jsx
+++ client/src/components/Ui/Ui.jsx
@@ -1,14 +1,11 @@
+import OptionsMenu from "../OptionsMenu/OptionsMenu";
+
 function Ui() {
 
     return (
         <section className="ui-menu">
           <CategoryMenu />
-        <section className="ui-menu__options">
-          <div className="ui-menu__options-start"></div>
-          <div className="ui-menu__options-end"></div>
-          <div className="ui-menu__options-reset"></div>
-          <div className="ui-menu__options-download"></div>
-        </section>
+        <OptionsMenu />
       </section>
     )
 }
