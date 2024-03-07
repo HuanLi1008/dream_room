948b765cf6b93ea453fa67b5af27ca7407e0a9cc
Heppner <Heppner@Rebekahs-MacBook-Pro.local>
2024-03-07 12:22:22 -0800

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,19 @@ import Scene from "./components/Scene/Scene";
 import { useState } from "react";
 
 function App() {
-  const [userChoice, setUserChoice] = useState([]);
+  const [userChoice, setUserChoice] = useState([{
+    "name": "window_blue",
+    "image": "http://localhost:8080/images/window/window_blue.png",
+    "id": 7,
+    "category": "window"
+  },]);
 
   return (
     <section className="game-container">
       <Scene userChoice={userChoice} />
       <section className="ui">
         <Ui userChoice={userChoice} setUserChoice={setUserChoice} />
-        <div className="ui-default-image"></div>
-        <div className="ui-default-image__empty"></div>
+        
       </section>
     </section>
   );
